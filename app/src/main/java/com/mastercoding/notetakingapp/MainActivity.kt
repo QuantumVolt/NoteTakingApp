080c11a7535def1e798d95c24d482445af610590
Chibwe Richard <rchibwe@gmail.com>
2026-07-13 21:19:53 +0200

    006_SourceCodeImplementation

--- app/src/main/java/com/mastercoding/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/mastercoding/notetakingapp/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.mastercoding.notetakingapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.mastercoding.notetakingapp.database.NoteDatabase
+import com.mastercoding.notetakingapp.databinding.ActivityMainBinding
+import com.mastercoding.notetakingapp.repository.NoteRepository
+import com.mastercoding.notetakingapp.viewmodel.NoteViewModel
+import com.mastercoding.notetakingapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var  noteViewModel : NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewModel()
+
+
+
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+
+        val viewModelProviderFactory = NoteViewModelFactory(application,
+            noteRepository)
+
+         noteViewModel = ViewModelProvider(
+             this,
+             viewModelProviderFactory)
+             .get(NoteViewModel::class.java)
     }
 }
