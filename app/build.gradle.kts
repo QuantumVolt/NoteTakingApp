5ea82cb0eba3710d406ba5b4c50fbe7ab24e0f07
Chibwe Richard <rchibwe@gmail.com>
2026-07-13 10:38:05 +0200

    001_ProjectSetup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,16 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.safe.args)
+
 }
 
 android {
     namespace = "com.mastercoding.notetakingapp"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -31,6 +36,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures{
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -39,7 +47,25 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    //Kotlin symbol processing KSP not kapt for kotlin Room-Compiler
+    implementation(libs.room.compiler.ksp)
+    implementation(libs.room.runtime)    //
+
+    //couroutines
+    implementation(libs.kotlin.couroutines)    //
+    //Navigation fragment
+    implementation(libs.navaigation.fragment)    //
+    implementation(libs.navigation.ui)    // Navigation
+    //lifecycle arch
+    implementation(libs.lifecycle.viewmodel.ktx)    // View model
+    implementation(libs.lifecycle.livedata.ktx)    // Live data
+    implementation(libs.lifecycle.runtime.ktx)    //
+    implementation(libs.lifecycle.compiler)    //
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+
+
 }
